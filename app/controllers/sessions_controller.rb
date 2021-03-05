0f5f41a7bcab8c2eb61b3e7482a65bb1653a79e7
Duc Tran <ducvtrann@gmail.com>
2021-03-05 17:59:04 -0500

    Added registration controller, logout and logged_in checkers, current_user concern

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
-class SessionController < ApplicationController
+class SessionsController < ApplicationController
+  include CurrentUserConcern
+
   def create
     user = User
             .find_by(email: params['user']['email'])
@@ -16,5 +18,18 @@ class SessionController < ApplicationController
         status: 401
       }
     end
+  end
+
+  def logged_in
+    if @current_user
+      render json: { logged_in: true, user: @current_user }
+    else
+      render json: { logged_in: false }
+    end
+  end
+
+  def logout
+    reset_session
+    render json: { status: 200, logged_out: true }
   end
 end
