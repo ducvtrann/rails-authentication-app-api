0f039601f47eb080491ede6b9eb7bc0c2b0f08f8
Duc Tran <ducvtrann@gmail.com>
2021-03-05 16:12:02 -0500

    Created user model, session_controller, and create session_controller#create

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,20 @@
+class SessionController < ApplicationController
+  def create
+    user = User
+            .find_by(email: params['user']['email'])
+            .try(:authenticate, params['user']['password'])
+
+    if user
+      session[:user_id] = user.id
+      render json: {
+        status: :created,
+        loggged_in: true,
+        user: user
+      }
+    else
+      render json: {
+        status: 401
+      }
+    end
+  end
+end
